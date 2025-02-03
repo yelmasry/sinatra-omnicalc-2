92cc6c312f3992cea0d48a33b467f0343c6ca8d6
yelmasry <yarra.elmasry@gmail.com>
2025-02-03 01:39:39 +0000

    numbers final

--- app.rb
+++ app.rb
@@ -5,40 +5,61 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+
+###ADD
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
+
+
+###SUBTRACT
 get("/subtract") do
   erb(:sub_form)
 end
 
 get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
 
+  @result = @first_num - @second_num
   erb(:sub_result)
 end
 
+
+
+###MULTIPLY
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+
+  @result = @first_num * @second_num 
+
   erb(:mult_result)
 end
 
+
+###DIVIDE 
 get("/divide") do
   erb(:div_form)
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+
+  @result = @first_num / @second_num 
   erb(:div_result)
 end
